e67f63858f1dcf7934550a4064f130cbecfa0f66
Rafael <rlunavasquez@gmail.com>
2025-12-24 19:56:37 -0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,7 @@
+URBAN_ROUTES_URL = 'https://cnt-96d74312-3ed9-4b37-8686-a1cd9d7ad5fa.containerhub.tripleten-services.com'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
