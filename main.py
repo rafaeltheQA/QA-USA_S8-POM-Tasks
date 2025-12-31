cacd909dd66a7cc89ec460f49d3e328ba19577d6
rafaelvasquez <rlunavasquez@gmail.com>
2025-12-30 19:05:46 -0800

    Project 8

--- main.py
+++ main.py
@@ -15,130 +15,99 @@ class TestUrbanRoutes:
         cls.driver = webdriver.Chrome()
 
         if is_url_reachable(data.URBAN_ROUTES_URL):
-            print("some success message here")
-            cls.driver.get(data.URBAN_ROUTES_URL)
+            print("Connected to Urban Routes server")
         else:
             print("Cannot connect to the Urban Routes. Check the server is on and and still running")
 
     def test_set_route(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
-        urban_routes_page.enter_locations("East 2nd Street, 601", "1300 1st St")
-        assert urban_routes_page.get_from_location() == "East 2nd Street, 601"
-        assert urban_routes_page.get_to_location() == "1300 1st St"
+        urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        assert urban_routes_page.get_from_location() == data.ADDRESS_FROM
+        assert urban_routes_page.get_to_location() == data.ADDRESS_TO
 
     def test_select_plan(self):
-       urban_routes_page = UrbanRoutesPage(self.driver)
-       urban_routes_page.enter_from_location("East 2nd Street, 601")
-       urban_routes_page.enter_to_location("1300 1st St")
-       urban_routes_page.taxi_icon_locator()
-       urban_routes_page.click_taxi_text_locator()
-       urban_routes_page.click_duration_text_locator()
-       urban_routes_page.click_call_taxi_button()
-       urban_routes_page.click_supportive_plan_button()
-       urban_routes_page.click_supportive_plan_select_locator()
-       actual_value = urban_routes_page.get_active_selection_value()
-       expected_value = "Supportive\n$8.35"
-       assert actual_value in expected_value, f"Expected {expected_value}' but got {actual_value}"
+        self.driver.get(data.URBAN_ROUTES_URL)
+        urban_routes_page = UrbanRoutesPage(self.driver)
+        urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        urban_routes_page.click_call_taxi_button()
+        urban_routes_page.click_supportive_plan_button()
+        urban_routes_page.get_active_selection_value()
+        actual_value = urban_routes_page.get_active_selection_value()
+        expected_value = "Supportive"
+        assert expected_value in actual_value, f"Expected {expected_value}' but got {actual_value}"
 
     def test_fill_phone_number(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
-        urban_routes_page.enter_from_location("East 2nd Street, 601")
-        urban_routes_page.enter_to_location("1300 1st St")
-        urban_routes_page.taxi_icon_locator()
-        urban_routes_page.click_taxi_text_locator()
-        urban_routes_page.click_duration_text_locator()
+        urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        urban_routes_page.click_call_taxi_button()
+        urban_routes_page.click_supportive_plan_button()
         urban_routes_page.click_phone_field()
-        urban_routes_page.enter_phone_number(self.driver)
+        urban_routes_page.enter_phone_number(data.PHONE_NUMBER)
         urban_routes_page.send_sms_button()
         urban_routes_page.enter_confirmation_code()
         actual_phone = urban_routes_page.get_phone_number()
-        expected_phone = "+1 123 123 12 12"
+        expected_phone = data.PHONE_NUMBER
         assert expected_phone in actual_phone, f"Expected '{expected_phone}', but got '{actual_phone}'"
 
     def test_fill_card(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
-        urban_routes_page.enter_from_location("East 2nd Street, 601")
-        urban_routes_page.enter_to_location("1300 1st St")
-        urban_routes_page.taxi_icon_locator()
-        urban_routes_page.click_taxi_text_locator()
-        urban_routes_page.click_duration_text_locator()
+        urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_call_taxi_button()
         urban_routes_page.click_supportive_plan_button()
-        urban_routes_page.click_supportive_plan_select_locator()
         urban_routes_page.click_payment_method()
         urban_routes_page.click_add_card_button()
-        urban_routes_page.enter_credit_card()
-        urban_routes_page.enter_credit_card_code()
-        urban_routes_page = UrbanRoutesPage(self.driver)
-        urban_routes_page.change_focus_from_code_field()
+        urban_routes_page.enter_credit_card(data.CARD_NUMBER)
+        urban_routes_page.enter_credit_card_code(data.CARD_CODE)
         urban_routes_page.click_link_button_locator()
-        urban_routes_page.verify_card_added_checkmark()
-        urban_routes_page.get_payment_method_text()
         assert urban_routes_page.get_payment_method_text() == "Card"
 
     def test_comment_for_driver(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
-        urban_routes_page.enter_from_location("East 2nd Street, 601")
-        urban_routes_page.enter_to_location("1300 1st St")
-        urban_routes_page.taxi_icon_locator()
-        urban_routes_page.click_taxi_text_locator()
-        urban_routes_page.click_duration_text_locator()
+        urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_call_taxi_button()
         urban_routes_page.click_supportive_plan_button()
-        urban_routes_page.click_supportive_plan_select_locator()
-        urban_routes_page.enter_comment(text='Stop at the juice bar, please')
+        urban_routes_page.enter_comment(text=data.MESSAGE_FOR_DRIVER)
         actual_value = urban_routes_page.get_driver_comment()
-        expected_value = "Stop at the juice bar, please"
-        assert actual_value == expected_value,  \
-            f"Expected '{expected_value}' but got '{actual_value}'"
+        expected_value = data.MESSAGE_FOR_DRIVER
+        assert actual_value == expected_value, f"Expected '{expected_value}' but got '{actual_value}'"
 
     def test_order_blanket_and_handkerchief(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
-        urban_routes_page.enter_from_location("East 2nd Street, 601")
-        urban_routes_page.enter_to_location("1300 1st St")
-        urban_routes_page.taxi_icon_locator()
-        urban_routes_page.click_taxi_text_locator()
-        urban_routes_page.click_duration_text_locator()
+        urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_call_taxi_button()
         urban_routes_page.click_supportive_plan_button()
-        urban_routes_page.click_supportive_plan_select_locator()
         urban_routes_page.click_blanket_toggle()
-        assert urban_routes_page.is_blanket_selected() is True, \
-            "Blanket and handkerchiefs toggle should be ON"
+        assert urban_routes_page.is_blanket_selected(),"Blanket and handkerchiefs toggle should be ON"
 
 
     def test_order_2_ice_cream(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
-        urban_routes_page.enter_from_location("East 2nd Street, 601")
-        urban_routes_page.enter_to_location("1300 1st St")
-        urban_routes_page.taxi_icon_locator()
-        urban_routes_page.click_taxi_text_locator()
-        urban_routes_page.click_duration_text_locator()
+        urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_call_taxi_button()
         urban_routes_page.click_supportive_plan_button()
-        urban_routes_page.click_supportive_plan_select_locator()
         urban_routes_page.order_ice_cream(times=2)
         ice_cream_count = urban_routes_page.get_ice_cream_count()
         assert ice_cream_count == "2", f"Expected cont 2 but got {ice_cream_count}"
 
     def test_car_search_model_appears(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
-        urban_routes_page.enter_from_location("East 2nd Street, 601")
-        urban_routes_page.enter_to_location("1300 1st St")
-        urban_routes_page.taxi_icon_locator()
-        urban_routes_page.click_taxi_text_locator()
-        urban_routes_page.click_duration_text_locator()
+        urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_call_taxi_button()
         urban_routes_page.click_supportive_plan_button()
-        urban_routes_page.click_supportive_plan_select_locator()
         urban_routes_page.click_phone_field()
         urban_routes_page.enter_phone_number(self.driver)
         urban_routes_page.send_sms_button()
         urban_routes_page.enter_confirmation_code()
-        actual_phone = urban_routes_page.get_phone_number()
-        expected_phone = "+1 123 123 12 12"
-        assert expected_phone in actual_phone, f"Expected '{expected_phone}', but got '{actual_phone}'"
-        urban_routes_page.enter_message_for_driver('Stop at the juice bar, please')
+        urban_routes_page.enter_message_for_driver(data.MESSAGE_FOR_DRIVER)
+        urban_routes_page.click_order_button()
+        assert urban_routes_page.is_car_search_modal_displayed(), "Car search modal should appear"
 
 
     @classmethod
--- pages.py
+++ pages.py
@@ -7,25 +7,22 @@ from selenium.webdriver.support.expected_conditions import element_to_be_clickab
 
 from helpers import retrieve_phone_code
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC, expected_conditions
+
 
 class UrbanRoutesPage:
     FROM_LOCATOR = (By.ID, 'from')
     TO_LOCATOR = (By.ID, 'to')
-    TAXI_ICON_LOCATOR = (By.XPATH, '//img[@src="/static/media/taxi-active.b0be3054.svg"]')
-    TAXI_TEXT_LOCATOR = (By.XPATH, '//div[@class="results-text"]//div[@class="text"]')
-    DURATION_TEXT_LOCATOR = (By.XPATH, '//div[@class="results-text"]//div[@class="duration"]')
     CALL_TAXI_BUTTON_LOCATOR = (By.XPATH, '//button[@class="button round"]')
-    CLICK_SUPPORTIVE_PLAN_BUTTON_LOCATOR = (By.XPATH, '//button[@class="button round"]')
-    SUPPORTIVE_PLAN_SELECTION_LOCATOR = (By. XPATH, '//div[contains(text(),"Supportive")]')
+    SUPPORTIVE_PLAN_BUTTON_LOCATOR = (By.XPATH, "//div[contains(text(), 'Supportive')]")
     ACTIVE_SELECTION_VALUE_LOCATOR = (By. CSS_SELECTOR, 'div[class="tcard active"]')
     PHONE_NUMBER_BUTTON_LOCATOR = (By.CSS_SELECTOR, 'div.np-text')
     PHONE_NUMBER_INPUT_LOCATOR = (By.ID, "phone")
     GET_PHONE_NUMBER_LOCATOR = (By.ID, "get_phone_number")
     SET_PHONE_CODE_LOCATOR = (By.ID, "set_phone_code")
-    SEND_SMS_BUTTON_LOCATOR = (By.XPATH, "//button[@class='button full' and normalize-space(text())='Send SMS']")
+    SEND_SMS_BUTTON_LOCATOR = (By.XPATH, "//button[contains(@class, 'button')]")
     CLICK_CONFIRM_BUTTON_LOCATOR = (By.XPATH, "//button[contains(text(),'Confirm')]")
-    CONFIRM_CODE_FIELD_LOCATOR = (By.ID, "sms_code")
+    CONFIRM_CODE_FIELD_LOCATOR = (By.ID, "code")
     CLICK_PAYMENT_METHOD_LOCATOR = (By.XPATH, "//div[@class='pp-text']")
     ADD_CARD_LOCATOR = (By.XPATH, "//div[contains(text(),'Add card')]")
     ENTER_CREDIT_CARD_LOCATOR = (By.ID, "number")
@@ -48,9 +45,13 @@ class UrbanRoutesPage:
         self.driver = driver
 
     def enter_from_location(self, from_text):
-        self.driver.find_element(*self.FROM_LOCATOR).send_keys(from_text)
+        wait = WebDriverWait(self.driver, 10)
+        from_field = wait.until(EC.element_to_be_clickable(self.FROM_LOCATOR))
+        from_field.send_keys(from_text)
     def enter_to_location(self, to_text):
-        self.driver.find_element(*self.TO_LOCATOR).send_keys(to_text)
+        wait = WebDriverWait(self.driver, 10)
+        to_field = wait.until(EC.element_to_be_clickable(self.TO_LOCATOR))
+        to_field.send_keys(to_text)
     def get_from_location(self):
         return self.driver.find_element(*self.FROM_LOCATOR).get_attribute("value")
     def get_to_location(self):
@@ -60,81 +61,40 @@ class UrbanRoutesPage:
             self.enter_from_location(from_text)
             self.enter_to_location(to_text)
 
-    def taxi_icon_locator(self):
-        wait = WebDriverWait(self.driver, timeout=10)
-        taxi_button = wait.until(EC.element_to_be_clickable(self.TAXI_ICON_LOCATOR))
-        if not taxi_button.get_attribute("class").__contains__("active"):
-            taxi_button.click()
-    def click_taxi_text_locator(self):
-        self.driver.find_element(*self.TAXI_TEXT_LOCATOR).click()
-    def click_duration_text_locator(self):
-        self.driver.find_element(*self.DURATION_TEXT_LOCATOR).click()
     def click_call_taxi_button(self):
-        self.driver.find_element(*self.CALL_TAXI_BUTTON_LOCATOR).click()
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.element_to_be_clickable(self.CALL_TAXI_BUTTON_LOCATOR))
+        element.click()
     def click_supportive_plan_button(self):
-        try:
-            current_value = self.get_active_selection_value()
+        wait = WebDriverWait(self.driver, 10)
+        supportive_plan: WebElement = wait.until(EC.element_to_be_clickable(self.SUPPORTIVE_PLAN_BUTTON_LOCATOR))
+        supportive_plan.click()
 
-            if current_value == "Supportive Plan":
-                print("Supportive Plan is already selected")
-                return
-
-            wait = WebDriverWait(self.driver, 10)
-            button = wait.until(
-                EC.element_to_be_clickable(self.SUPPORTIVE_PLAN_SELECTION_LOCATOR)
-            )
-            button.click()
-
-        except TimeoutException:
-            raise Exception("Supportive Plan was not clickable within 10 seconds.")
-
-        except NoSuchElementException:
-            raise Exception("Supportive Plan was not found on the page.")
-
-        except Exception as e:
-            raise Exception(f"Unexpected error while clicking Supportive Plan: {e}")
-    def click_supportive_plan_select_locator(self):
-            wait = WebDriverWait(self.driver, 10)
-            button = wait.until(EC.element_to_be_clickable(self.SUPPORTIVE_PLAN_SELECTION_LOCATOR))
-            button.click()
     def get_active_selection_value(self):
             wait = WebDriverWait(self.driver, 10)
             element = wait.until(EC.presence_of_element_located(self.ACTIVE_SELECTION_VALUE_LOCATOR))
             return element.text
 
     def click_phone_field(self):
-        print("DEBUG: About to click phone field")
-        try:
-            # First, try to find and click normally
-            element = self.driver.find_element(*self.PHONE_NUMBER_BUTTON_LOCATOR)
-            self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
-            time.sleep(0.5)
-            element.click()
-            print("DEBUG: Phone field clicked successfully")
-        except Exception as e:
-            print(f"DEBUG: Normal click failed: {e}")
-            # If normal click fails, try JavaScript click
-            element = self.driver.find_element(*self.PHONE_NUMBER_BUTTON_LOCATOR)
-            self.driver.execute_script("arguments[0].click();", element)
-            print("DEBUG: Phone field clicked with JavaScript")
+        element = self.driver.find_element(*self.PHONE_NUMBER_BUTTON_LOCATOR)
+        element.click()
     def enter_phone_number(self, phone_number):
-        phone_input = self.driver.find_element(*self.PHONE_NUMBER_INPUT_LOCATOR)
+        wait = WebDriverWait(self.driver, 10)
+        phone_input = wait.until(EC.element_to_be_clickable(self.PHONE_NUMBER_INPUT_LOCATOR))
         phone_input.clear()
-        phone_input.send_keys("+1 123 123 12 12")
+        phone_input.send_keys(phone_number)
     def send_sms_button(self):
-        try:
-            iframe = self.driver.find_element(By. ID, "iframe-id")
-            self.driver.switch_to.frame(iframe)
-            wait = WebDriverWait(self.driver, 10)
-            send_btn = wait.until(EC.element_to_be_clickable(self.SEND_SMS_BUTTON_LOCATOR))
-            send_btn.click()
-        except Exception:
-            pass
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.element_to_be_clickable(self.SEND_SMS_BUTTON_LOCATOR))
+        element.click()
     def enter_confirmation_code(self):
-        code = retrieve_phone_code
-        assert code, "SMS confirmation code was not retrieved"
+        code = retrieve_phone_code(self.driver)
+        sms_input = self.driver.find_element(*self.CONFIRM_CODE_FIELD_LOCATOR)
+        sms_input.send_keys(code)
+
     def get_phone_number(self):
-        return self.driver.find_element(*self.PHONE_NUMBER_INPUT_LOCATOR).get_attribute("value")
+        self.driver.find_element(*self.GET_PHONE_NUMBER_LOCATOR).click()
+
     def click_payment_method(self):
         self.driver.find_element(*self.CLICK_PAYMENT_METHOD_LOCATOR).click()
     def click_add_card_button(self):
@@ -142,7 +102,7 @@ class UrbanRoutesPage:
         element = wait.until(EC.element_to_be_clickable(self.ADD_CARD_LOCATOR))
         element.click()
 
-    def enter_credit_card(self):
+    def enter_credit_card(self, card_number):
         wait = WebDriverWait(self.driver, 10)
         element = wait.until(EC.element_to_be_clickable(self.ENTER_CREDIT_CARD_LOCATOR))
         element.click()
@@ -150,32 +110,14 @@ class UrbanRoutesPage:
         element.send_keys('1234 5678 9100')
         element.send_keys(Keys.TAB)
 
-    def enter_credit_card_code(self):
-        time.sleep(2)
-        print("About to look for credit card code field...")
+    def enter_credit_card_code(self, code):
         wait = WebDriverWait(self.driver, 15)
-        element = wait.until(EC.visibility_of_element_located(self.ENTER_CREDIT_CARD_LOCATOR))
         element = wait.until(EC.element_to_be_clickable(self.ENTER_CREDIT_CARD_CODE_LOCATOR))
         print("Found code field successfully!")
         element.send_keys('1111')
         print("Entered code successfully!")
         element.send_keys(Keys.TAB)
 
-    def change_focus_from_code_field(self):
-        code_field = self.driver.find_element(*self.CODE_FIELD_INPUT_LOCATOR)
-        code_field.send_keys(Keys.TAB)
-    def click_outside_to_change_focus(self):
-        self.driver.find_element(By. TAG_NAME, "body").click()
-    def wait_for_link_button_clickable(self):
-        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.LINK_BUTTON_LOCATOR))
-    def click_link_button_locator(self):
-        wait = WebDriverWait(self.driver, 10)
-        element = wait.until(EC.element_to_be_clickable(self.LINK_BUTTON_LOCATOR))
-        element.click()
-    def verify_card_added_checkmark(self):
-        wait = WebDriverWait(self.driver, 10)
-        element = wait.until(EC.visibility_of_element_located(self.VERIFY_CARD_ADDED_LOCATOR))
-        element.click()
     def get_payment_method_text(self):
         return self.driver.find_element(*self.PAYMENT_METHOD_VALUE).text
 
@@ -190,19 +132,10 @@ class UrbanRoutesPage:
     def is_blanket_selected(self):
         return self.driver.find_element(*self.BLANKET_TOGGLE_INPUT).get_property("checked")
 
-    def order_ice_cream(self, times=1):
+    def order_ice_cream(self, times):
         plus_btn = self.driver.find_element(*self.ICE_CREAM_PLUS_BTN)
         for _ in range(times):
             plus_btn.click()
     def get_ice_cream_count(self):
         """Retrieve the displayed ice cream count"""
         return self.driver.find_element(*self.ICE_CREAM_COUNT).text
-
-    def enter_message_for_driver(self, message):
-        self.driver.find_element(*self.MESSAGE_TO_DRIVER_INPUT).send_keys(message)
-
-
-
-
-
-
